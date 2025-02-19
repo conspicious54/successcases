191b26d869f294181137f4b5b49713b342ae7124
conspicious54 <connor.wbyers@gmail.com>
2025-02-19 16:08:28 -0500

    Updated 5 files [BoltSync]

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 8080
-  },
-  preview: {
-    port: 8080
-  },
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true,
     rollupOptions: {
       output: {
         manualChunks: {
